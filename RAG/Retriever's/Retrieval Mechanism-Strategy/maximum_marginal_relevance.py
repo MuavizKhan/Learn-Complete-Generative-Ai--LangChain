4731cb4f844640fbbf54266cd60125f36afcf08d
MuavizKhan <muavizmaroof@gmail.com>
2026-09-24 19:41:01 +0530

    another technique in Retrievers - different types of results.

---
+++ RAG/Retriever's/Retrieval Mechanism-Strategy/maximum_marginal_relevance.py
@@ -0,0 +1,41 @@
+from langchain_community.vectorstores import FAISS
+from langchain_core.documents import Document
+from langchain_huggingface import HuggingFaceEmbeddings
+
+# Sample Documents
+docs = [
+    Document(page_content="LangChain makes it easy to work with LLMs."),
+    Document(page_content="LangChain is used to build LLM based applications."),
+    Document(page_content="Chroma is used to store and search document embeddings."),
+    Document(page_content="Embeddings are vector representations of text."),
+    Document(page_content="MMR helps you get diverse results when doing similarity search."),
+    Document(page_content="LangChain supports Chroma, FAISS, Pinecone, and more."),
+]
+
+# initilize embeddings.
+embedding_model = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+# Create the FAISS vector store from documents
+vectorstore = FAISS.from_documents(
+    documents=docs,
+    embedding=embedding_model
+)
+
+# Enable MMR in the retriever
+retriever = vectorstore.as_retriever(
+    search_type="mmr",   # <-- This enables MMR
+    search_kwargs={
+        "k": 3,                  # k = top results, lambda_mult = relevance-diversity balance
+        "lambda_mult": 0.1
+    }
+)
+
+query = "What is LangChain?"
+
+results = retriever.invoke(query)
+
+for i, doc in enumerate(results):
+    print(f"\n--- Result {i + 1} ---")
+    print(doc.page_content)
